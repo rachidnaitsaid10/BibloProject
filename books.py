276427d468486beca71fd9075a8386af966e98fe
rachidnaitsaid10 <rachidnaitsaid10@gmail.com>
2025-09-28 21:57:56 +0100

    add books function in main

--- books.py
+++ books.py
@@ -9,7 +9,7 @@ def SortingData(Mylist):
     Mylist.sort(key=lambda x: x["année"])
     return Mylist
     print("-" * 220)
-print(SortingData(livres))
+
 
 
 def Ancien(Mylist):
@@ -17,14 +17,14 @@ def Ancien(Mylist):
     AncienLiver = SotoringList[0]
     print(AncienLiver)
     print("-" * 220)
-Ancien(livres)
+
 
 def Récent(Mylist):
     SotoringList = SortingData(Mylist)
     RécentLiver = SotoringList[-1]
     print(RécentLiver)
     print("-" * 220)
-Récent(livres)
+
 
 
 def CreationDict(Mylist):
@@ -36,7 +36,7 @@ def CreationDict(Mylist):
             dictionnaire[item[1]] += 1
     print(dictionnaire)
     print("-" * 220)
-CreationDict(aime_livres)
+
 
 def PaginationList(Mylist):
     x=0
@@ -48,4 +48,4 @@ def PaginationList(Mylist):
         x += 2
     print("-" * 220)
 
-PaginationList(utilisateurs)
+
---
+++ main.py
@@ -0,0 +1,13 @@
+from Data import *
+from books import *
+
+print(utilisateurs)
+print(aime_livres)
+print(livres)
+print("-"*220)
+
+print(SortingData(livres))
+Ancien(livres)
+Récent(livres)
+CreationDict(aime_livres)
+PaginationList(utilisateurs)
